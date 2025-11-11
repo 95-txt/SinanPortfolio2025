d9dee65d98c3cf5dc8202b374c284ec8d2820555
95-txt <sinanlw95@gmail.com>
2025-11-12 00:18:44 +0530

    12,11

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Designfolio from "./components/Designfolio";
 import Logofolio from "./components/Logofolio";
 import Uiuxfolio from "./components/Uiuxfolio";
 import Artfolio from "./components/Artfolio";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [theme, setTheme] = useState(false);
@@ -22,29 +23,58 @@ function App() {
   };
 
   return (
-    <div
-      className={` ${
-        theme ? "" : "dark"
-      } bg-zinc-100/0 dark:bg-zinc-900/0 text-zinc-900 dark:text-zinc-100`}
-    >
-      <ParticlesBackground theme={theme} />
-      <Navbar onToggle={handleTheme} />
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/projects" element={<Projects />}>
-          <Route index element={<Navigate to="dev" replace />} />
-          <Route path="dev" element={<Devfolio />} />
-          <Route path="design" element={<Designfolio />} />
-          <Route path="logo" element={<Logofolio />} />
-          <Route path="uiux" element={<Uiuxfolio />} />
-          <Route path="art" element={<Artfolio />} />
-        </Route>
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        <title>Muhammed Sinan | Portfolio</title>
+        <meta
+          name="description"
+          content="Computer Science and engineering graduate, MERN Stack developer and Designer. Explore my portfolio website."
+        />
+        <meta
+          name="keywords"
+          content="Muhammed Sinan, portfolio, developer, graphic, designer, Calicut, UI UX, React, snnTorch, full stack, web developer, spiking neural networks, MERN, sinan, TKM, TKMCE"
+        />
+        <meta name="author" content="Muhammed Sinan" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Muhammed Sinan | Portfolio" />
+        <meta
+          property="og:description"
+          content="Computer Science and engineering graduate, MERN Stack developer and Designer. Explore my portfolio website."
+        />
+        <meta
+          property="og:url"
+          content="https://sinanportfolio2025.onrender.com/"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://sinanportfolio2025.onrender.com/preview.jpg"
+        />
+      </Helmet>
+      <div
+        className={` ${
+          theme ? "" : "dark"
+        } bg-zinc-100/0 dark:bg-zinc-900/0 text-zinc-900 dark:text-zinc-100`}
+      >
+        <ParticlesBackground theme={theme} />
+        <Navbar onToggle={handleTheme} />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/projects" element={<Projects />}>
+            <Route index element={<Navigate to="dev" replace />} />
+            <Route path="dev" element={<Devfolio />} />
+            <Route path="design" element={<Designfolio />} />
+            <Route path="logo" element={<Logofolio />} />
+            <Route path="uiux" element={<Uiuxfolio />} />
+            <Route path="art" element={<Artfolio />} />
+          </Route>
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 }
 
